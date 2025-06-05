687b03eafecfb2d0c98327d4816ffe7e967e1a97
Pavel <pavelmalyv@gmail.com>
2025-06-05 17:47:35 +0300

    feat(TodoListContainer): добавить компонент TodoListContainer

---
+++ src/components/sections/TodosListContainer.tsx
@@ -0,0 +1,11 @@
+import TodosList from '../layout/TodosList';
+
+const TodosListContainer = () => {
+	return (
+		<TodosList>
+			<TodosList.Item>Тестовое задание</TodosList.Item>
+		</TodosList>
+	);
+};
+
+export default TodosListContainer;
