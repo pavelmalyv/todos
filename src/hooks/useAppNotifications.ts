6074a40dbe5c8ed7e1245618ae36c5a9a6152f1a
Pavel <pavelmalyv@gmail.com>
2025-06-06 17:26:50 +0300

    refactor(useAppNotifications): добавить кеширование функций

--- src/hooks/useAppNotifications.ts
+++ src/hooks/useAppNotifications.ts
@@ -1,30 +1,40 @@
 import type { NotificationKey } from '@/types/notification';
 import { useNotifications } from '@toolpad/core/useNotifications';
+import { useCallback } from 'react';
 
 const HIDE_DURATION = 3000;
 
 const useAppNotifications = () => {
 	const notifications = useNotifications();
 
-	const showError = (message: string) => {
-		return notifications.show(message, {
-			severity: 'error',
-			autoHideDuration: HIDE_DURATION,
-		});
-	};
+	const showError = useCallback(
+		(message: string) => {
+			return notifications.show(message, {
+				severity: 'error',
+				autoHideDuration: HIDE_DURATION,
+			});
+		},
+		[notifications],
+	);
 
-	const showSuccess = (message: string) => {
-		return notifications.show(message, {
-			severity: 'success',
-			autoHideDuration: 3000,
-		});
-	};
+	const showSuccess = useCallback(
+		(message: string) => {
+			return notifications.show(message, {
+				severity: 'success',
+				autoHideDuration: 3000,
+			});
+		},
+		[notifications],
+	);
 
-	const closeNotification = (key: NotificationKey) => {
-		if (key !== null) {
-			notifications.close(key);
-		}
-	};
+	const closeNotification = useCallback(
+		(key: NotificationKey) => {
+			if (key !== null) {
+				notifications.close(key);
+			}
+		},
+		[notifications],
+	);
 
 	return { showError, showSuccess, closeNotification };
 };
